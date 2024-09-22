e029855c489427d6364e2df52fc648fbdf0c943f
Kashmain Tahir <kashmaint@student.unimelb.edu.au>
2024-09-22 15:55:37 +1000

    added env file for firebase configuration

--- firebaseFiles/firebaseConfig.js
+++ firebaseFiles/firebaseConfig.js
@@ -4,16 +4,14 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB9uQ1k6qNzAYXYyKNcWYxDvsFflYETK98",
-  authDomain: "sdg-scholar.firebaseapp.com",
-  projectId: "sdg-scholar",
-  storageBucket: "sdg-scholar.appspot.com",
-  messagingSenderId: "118706304555",
-  appId: "1:118706304555:web:77fb71aeb467bb673fdc6e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.VITE_FIREBASE_APP_ID,
 };
 
 // initialize Firebase
