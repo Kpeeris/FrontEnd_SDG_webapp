355d7d09ab88b993a52be4bfb177ffb7bb9c9579
kaylynthomso <kaylynthomson.phan@student.unimelb.edu.au>
2024-10-09 12:17:02 +1100

    feat: login back end logic

--- src/pages/login/components/LoginForm.jsx
+++ src/pages/login/components/LoginForm.jsx
@@ -1,17 +1,18 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { forwardRef } from "react";
 
-export const LoginForm = () => {
+export const LoginForm = forwardRef(({emailRef, passwordRef}, ref) => {
   return (
     <div className="space-y-6 mt-4 mb-2">
       <div className="w-full flex flex-col items-left gap-2">
         <Label htmlFor="email">Email</Label>
-        <Input type="email" id="email" placeholder="Email" />
+        <Input type="email" id="email" placeholder="Email" ref={emailRef}/>
       </div>
       <div className="w-full flex flex-col items-left gap-2">
         <Label htmlFor="password">Password</Label>
-        <Input type="password" id="password" placeholder="Password" />
+        <Input type="password" id="password" placeholder="Password" ref={passwordRef}/>
       </div>
     </div>
   );
-};
+});
